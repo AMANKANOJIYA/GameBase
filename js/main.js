9857ea1fabc6ae452b1497e8f4a2f91993c43c47
AMANKANOJIYA <aman.kanojiya4203@gmail.com>
2021-02-25 22:36:04 +0530

    add rebiew

--- js/main.js
+++ js/main.js
@@ -19,20 +19,20 @@ firebase.auth().onAuthStateChanged(function(user) {
   firebaseref.once("value",(snapshot)=>{
     var data=snapshot.val();
     for (let i in data){
-      let element=`<div class="card " id="card-${data[i]['Name']}">
-      <div class="immage_con">
+      let element=`<div class="card unselectebal" id="card-${data[i]['Name']}">
+      <div class="immage_con unselectebal">
           <img src="photos/bg-story.7.jpg" alt="IMAGE NOT FOUND">
           <div>${data[i]['Tag']}</div>
       </div>
-      <div class="game_con">
-          <div class="game_name">
+      <div class="game_con unselectebal">
+          <div class="game_name unselectebal">
           ${data[i]['Name']}
           </div>
-          <div class="game_info">
-              <div class="rate">${data[i]['Rate']}   rating</div>
-              <div class="views" style="color: #9d9d9d;"><span style="color: #4025FB; font-weight: 600;">${data[i]['Views']}</span> Views </div>
+          <div class="game_info unselectebal">
+              <div class="rate unselectebal">${data[i]['Rate']}   rating</div>
+              <div class="views unselectebal" style="color: #9d9d9d;"><span style="color: #4025FB; font-weight: 600;">${data[i]['Views']}</span> Views </div>
           </div>
-          <div class="imp_btn">
+          <div class="imp_btn unselectebal">
               <a href="${data[i]['Link']}">Play Now</a>
               <a href="">Review</a>
           </div>
---
+++ js/review.js
@@ -0,0 +1,16 @@
+// signout system----------------------------------------------
+function logout_l(){
+    firebase.auth().signOut().then(() => {
+    window.location.assign("http://127.0.0.1:5500/login.html")
+    }).catch((error) => {
+    console.log(error)
+})}
+// auth check if user sign up or loged in
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+    } else {
+      // No user is signed in.
+      window.location.assign("http://127.0.0.1:5500/login.html")
+    }
+  });
