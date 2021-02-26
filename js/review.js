6aaeaa552fb0b6a9bf74abceb0428bb24a9233e4
AMANKANOJIYA <aman.kanojiya4203@gmail.com>
2021-02-26 23:24:46 +0530

    add comment

--- js/review.js
+++ js/review.js
@@ -14,3 +14,26 @@ firebase.auth().onAuthStateChanged(function(user) {
       window.location.assign("http://127.0.0.1:5500/login.html")
     }
   });
+//   comment add and featuch
+let comment=document.getElementById("comment");
+
+function ready(){
+    comment=comment.value;
+    let user=firebase.auth().currentUser;
+    let email=user.email;
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+    console.log(dateTime,comment,email)
+}
+const comment_btn=document.getElementById("comment_btn");
+comment_btn.onclick=()=>{
+    ready();
+    // firebase.database.ref("Comment/"+id).set({
+    //     Time:date,
+    //     comment:commment,
+    //     game_id:,
+    //     user:email
+    // })
+}
