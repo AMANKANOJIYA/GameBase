1b90bb11d39312fcfafde267720eac7917122bd8
AMANKANOJIYA <aman.kanojiya4203@gmail.com>
2021-02-28 22:20:45 +0530

    add comment system

--- js/review.js
+++ js/review.js
@@ -20,49 +20,41 @@ var url = new URL(url_string);
 var game_id = url.searchParams.get("game_id");
 // ======================================================= 
 //   comment add and featuch
-let comment=document.getElementById("comment");
-function ready(){
-  // comment from element
-    comment=comment.value;
+function addcomment(){
     // firebase user featuch
     let user=firebase.auth().currentUser;
     let email=user.email;
-    var firebaseref=firebase.database().ref("user");
-    firebaseref.once("value",(snapshot)=>{
-      var data=snapshot.val();
-      for (let i in data){
-        if (data[i]["email"]==email){
-          let main_c=data[i]["user_name"]
-          break
-        }
-      }
-    })
-    console.log(main_c)
     // date and time
     let today = new Date();
     let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
     let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     let dateTime = date+' '+time;
-    return [comment,dateTime]
-}
-const comment_btn=document.getElementById("comment_btn");
-comment_btn.onclick=()=>{
-    let data= ready();
-    var postListRef = firebase.database().ref('Comment');
-    var newPostRef = postListRef.push();
-    console.log(document.getElementById("comment_user"))
-    console.log(data)
-    newPostRef.set({
-        Time:data[1],
-        comment:data[0],
-        game_id:game_id,
-        user:username
-    });
+    firebase.database().ref("user").once("value").then(function(element){
+    // comment from element
+    let comment=document.getElementById("comment");
+    comment=comment.value;
+    console.log(comment)
+      let data=element.val()
+      var postListRef = firebase.database().ref('Comment');
+      var newPostRef = postListRef.push();
+      console.log(document.getElementById("comment_user"))
+      for (const key in data) {
+        if (data[key]["email"]==email) {
+          newPostRef.set({
+          Time:dateTime,
+          Comment:comment,
+          game_id:game_id,
+          user:data[key]["user_name"]
+        });
+        console.log(dateTime,comment,game_id,data[key]["user_name"])
+        }
+      }
+    })
 }
 
 let some_comment=document.getElementById("some_comment");
 var firebaseref=firebase.database().ref("Comment");
-  firebaseref.on("value",(snapshot)=>{
+  firebaseref.once("value",(snapshot)=>{
     var data=snapshot.val();
     for (let i in data){
       if (data[i]["game_id"]==game_id) {
---
+++ js/test.js
@@ -0,0 +1,9 @@
+let obj={
+    aman:"thia ia aman",
+    chaman:"",
+    gaman:""
+}
+function ravi(){
+    obj.chaman="this is a a a a a a"
+    obj.gaman="dafjjuahfdhfi"
+}
